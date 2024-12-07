c54381b2c1e8aa53e39ff8ad6a059cb7a2128c56
Andreas Adam <andreas.adam@parcellab.com>
2024-12-07 14:49:16 +0100

    refactor: init tanstack start w tailwind

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+// app.config.ts
+import { defineConfig } from "@tanstack/start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+export default defineConfig({
+    vite: {
+        plugins: [
+            tsConfigPaths({
+                projects: ["./tsconfig.json"],
+            }),
+        ],
+    },
+});
---
+++ app/routes/__root.tsx
@@ -0,0 +1,72 @@
+// app/routes/__root.tsx
+import {
+	Outlet,
+	ScrollRestoration,
+	createRootRoute,
+} from "@tanstack/react-router";
+import { Meta, Scripts } from "@tanstack/start";
+import type { ReactNode } from "react";
+import appCss from "@/styles/app.css?url";
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "TanStack Start Starter",
+			},
+		],
+        links: [
+			{ rel: "stylesheet", href: appCss },
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/favicon-32x32.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				href: "/favicon-16x16.png",
+			},
+			{ rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+			{ rel: "icon", href: "/favicon.ico" },
+		],
+	}),
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
